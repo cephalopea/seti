4aaa460a201798191872bd3848670f31007c792c
DONGUS3 <ballen.kun@gmail.com>
2019-04-24 16:21:50 -0400

    civ() Class Declaration and Global Variables
    This is a class declaration for a civilization, civ().
    You can specify values by saying civ(Arms_Deployment_Speed = 0.05), etc. Otherwise, the default is our civilization, which can launch interstellar weapons at 0.00025 times the speed of light, ranks 0.7 on the Kardashev scale, and has an 50% aggression score. The aggression score and especially the Communication constant (currently 1) are open to change, as they are arbitrary variables. I'd make an argument that we are a good benchmark for an average level of aggressiveness in a civilization at our level of development, or at least our sample size of 1 makes it difficult to make this value anything other than 0.5 (for now).

---
+++ SpaceNukes.py
@@ -0,0 +1,77 @@
+import numpy as np
+import random as rd
+
+##### Global Constants #####
+
+# Development
+#The level of the civilization's development,
+#described by its placement on the Kardashev scale.
+# A range between 0.0 and 2.0 for the purposes of this project.
+Development = 0.7
+
+# Communication
+#Communication constant. The ratio between the Development of a civilization and
+#its ability to send and recieve signals from other civilizations.
+Communication = 1.0
+
+# Speed
+#The default ratio between the speed of light and the speed of a civilization's
+#weapons of mass destruction.
+#Number is the top speed of Juno, divided by the speed of light
+Speed = 0.00025
+
+# Aggression
+#A score representing a civilization's tendencies towards aggression.
+#A score of 0.0 represents a civilization that never acts aggressively.
+#A score of 1.0 represents a civilization that always acts aggressively.
+Aggression = 0.5
+
+
+##### Civilization Class #####
+
+class civ:
+    # Development
+    #The level of the civilization's development,
+    #described by its placement on the Kardashev scale.
+    # A range between 0.0 and 2.0 for the purposes of this project.
+    dev = Development
+
+    # Arms Deployment Speed
+    #The speed of a weapon launched by this civilization
+    #In terms of a fraction of the speed of light
+    arms = Speed
+
+    # Communication Score
+    #A score representing a civilization's ability to communicate with other civilizations.
+    comm = Communication * dev
+
+    # Aggression
+    #A score representing a civilization's tendencies towards aggression.
+    #A score of 0.0 represents a civilization that never acts aggressively.
+    #A score of 1.0 represents a civilization that always acts aggressively.
+    agg = Aggression
+
+    def __init__(self, Devel = None, Arms_Deployment_Speed = None, Aggressiveness = None, Communication_Score = None):
+        if Devel is None:
+            self.dev = Development
+        else:       
+            self.dev = Devel
+        if Arms_Deployment_Speed is None:
+            self.arms = Speed
+        else:    
+            self.arms = Arms_Deployment_Speed
+        if Aggressiveness is None:
+            self.agg = Aggression
+        else:    
+            self.agg = Aggressiveness
+        if Communication_Score is None:
+            self.comm = Communication * self.dev
+        else:    
+            self.comm = Communication_Score
+
+
+
+
+# Earth has a development value of 0.7.
+Earth = civ()
+print(Earth.comm)
