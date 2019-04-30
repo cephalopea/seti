ea649303ce062f967b0b30dbcd019d64de5ffffe
Caiti Hensley <caiti@hampguest69-248.hampshire.edu>
2019-04-30 12:50:19 -0400

    main shell

---
+++ code/main.py
@@ -0,0 +1,28 @@
+import numpy as np
+import random as rd
+import SpaceNukes
+
+civ1Dev = None
+civ1Arms = None
+civ1Agg = None
+civ1Comm = None
+
+civ2Dev = None
+civ2Arms = None
+civ2Agg = None
+civ2Comm = None
+
+civDistance = 1 #in lightyears??? dunno yet, we'll get there
+
+def GetCustomCivSettings():
+    #get user input for civ 1 and civ 2 traits and assign it to vars above, plus distance?
+    
+def Communicate():
+    #one round between the civs
+
+def Main(getUserInput):
+    if (getUserInput):
+        GetCustomCivSettings()
+    civ1 = SpaceNukes.civ(civ1Dev, civ1Arms, civ1Agg, civ1Comm)
+    civ2 = SpaceNukes.civ(civ2Dev, civ2Arms, civ2Agg, civ2Comm)
+    #do we need something for distance? how do we know how many times civs can communicate?
