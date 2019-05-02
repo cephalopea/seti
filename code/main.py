3cbfce4ebb39317f40376790a790f0ee78353368
pterodactylogan <loganswan37@gmail.com>
2019-05-01 23:21:47 -0400

    created several graphs in data folder

--- code/main.py
+++ code/main.py
@@ -1,3 +1,4 @@
+import threading
 import numpy as np
 import random as rd
 import pandas as pd
@@ -29,6 +30,21 @@ def GetCustomCivSettings():
     
     print("Distance Between Civilizations (in lightyears): ")
     civDistance = input()
+    
+    return {"civ1": {"dev": civ1Dev, "arms": civ1Arms, "agg": civ1Agg, "comm": civ1Comm}, "civ2": {"dev": civ2Dev, "arms": civ2Arms, "agg": civ2Agg, "comm": civ2Comm}, "dist": civDistance}
+
+def GetStandardCivSettings():
+    civ1Dev = 1
+    civ1Arms = 0.2
+    civ1Agg = 1
+    civ1Comm = 1
+
+    civ2Dev = 1
+    civ2Arms = 0.2
+    civ2Agg = 1
+    civ2Comm = 1
+    
+    civDistance = 5
     
     return {"civ1": {"dev": civ1Dev, "arms": civ1Arms, "agg": civ1Agg, "comm": civ1Comm}, "civ2": {"dev": civ2Dev, "arms": civ2Arms, "agg": civ2Agg, "comm": civ2Comm}, "dist": civDistance}
 
@@ -117,86 +133,137 @@ def Main(getUserInput):
         print("Error Encountered")
     return True
 
+def runTrials(param):
+    print("runTrials of "+param)
+    data=[["civ1 dev"],["civ1 arms"],["civ1 agg"],["civ1 com"],["civ2 dev"],["civ2 arms"],["civ2 agg"],["civ2 com"],["dist"],["rounds"],["outcome"]]
+    for i in range(20):
+        if(i%2==0):
+            print(param, i)
+        for j in range(20):
+            civInputs = GetStandardCivSettings()
+            civ1 = civilization.civ(civInputs["civ1"]["dev"], civInputs["civ1"]["arms"], civInputs["civ1"]["agg"], civInputs["civ1"]["comm"]) #init civ 1 using values (default or user input)
+            civ2 = civilization.civ(civInputs["civ2"]["dev"], civInputs["civ2"]["arms"], civInputs["civ2"]["agg"], civInputs["civ2"]["comm"]) #init civ 2 using values (default or user input)
+            
+            civDistance = civInputs["dist"]
+
+            if(param=="dist"):
+                civDistance=i//2
+            elif(param=="dev"):
+                civ1.dev=i/10
+                civ2.dev=j/10
+            elif(param=="arms"):
+                civ1.arms=1-math.sqrt(i/20)
+                civ2.arms=1-math.sqrt(j/20)
+            elif(param=="agg"):
+                civ1.agg=i/20
+                civ2.agg=j/20
+            elif(param=="comm"):
+                civ1.comm=i/10
+                civ2.comm=j/10
+            elif(param=="randomize"):
+                civInputs=GetRandomCivSettings()
+                civ1 = civilization.civ(civInputs["civ1"]["dev"], civInputs["civ1"]["arms"], civInputs["civ1"]["agg"], civInputs["civ1"]["comm"]) #init civ 1 using values (default or user input)
+                civ2 = civilization.civ(civInputs["civ2"]["dev"], civInputs["civ2"]["arms"], civInputs["civ2"]["agg"], civInputs["civ2"]["comm"]) #init civ 2 using values (default or user input)
+            
+                civDistance = civInputs["dist"]
+            else:
+                print("invalid parameter")
+                return
+
+            data[0].append(civ1.dev)
+            data[1].append(civ1.arms)
+            data[2].append(civ1.agg)
+            data[3].append(civ1.comm)
+
+            data[4].append(civ2.dev)
+            data[5].append(civ2.arms)
+            data[6].append(civ2.agg)
+            data[7].append(civ2.comm)
+
+            data[8].append(civDistance)
+            
+            currentRound = 0
+            consecutiveComm = 0
+            attackRounds = 0 #instantiate number of rounds
+            if (civ1.arms < civ2.arms): #if civ 1 is slower than civ 2
+                attackRounds = math.ceil(civDistance/civ1.arms) #rounds for the slower civ to nuke the faster civ
+            else: #otherwise civ 2 is slower
+                attackRounds = math.ceil(civDistance/civ2.arms) #same as above
+            #this while condition is really long:
+            #checks that neither civ is dead, we haven't timed out, and we haven't established trust
+            while (civ1.dev > 0) and (civ2.dev > 0) and (currentRound < 10*attackRounds) and (consecutiveComm < attackRounds):
+                #check to see if any pending actions are realized this turn
+                civ1Actionable = filter(interaction.CheckPending, civ1.pending) #find the items that are happening this turn
+                for action in civ1Actionable: #for each of those items
+                    civ1.dev += action["impact"] #add their impact (pos or neg) to development
+                civ2Actionable = filter(interaction.CheckPending, civ2.pending) #do same steps for civ 2
+                for action in civ2Actionable:
+                    civ2.dev += action["impact"]
+                    
+                #once development scores are altered, decide what to do next
+                est = decision.ChooseActions(civ1, civ2) #get the decisions of each civ
+                
+                #keep track of consecutive communication count
+                if ((est[0] == "communicate") and (est[1] == "communicate")): #if both communicated
+                    consecutiveComm += 1 #increase their comm count
+                else: #if one attacked
+                    consecutiveComm = 0 #zero out comm count
+                    
+                #handle actions for this turn
+                if (est[0] == "attack"): #civ 1 attacked
+                    civ2.pending.append(interaction.Attack(civ1, civDistance)) #add an attack to civ 2's pending
+                else: #civ 1 communicated
+                    civ2.pending.append(interaction.Communicate(civ1, civDistance)) #add a communication to civ 2's pending
+                if(est[1] == "attack"): #civ 2 attacked
+                    civ1.pending.append(interaction.Attack(civ2, civDistance)) #add an attack to civ 1's pending
+                else:
+                    civ1.pending.append(interaction.Communicate(civ2, civDistance)) #add a communication to civ 1's pending
+                    
+                currentRound += 1 #increment current round for timeout
+                    
+            #the game has concluded, figure out why
+            data[9].append(currentRound)
+            if (civ1.dev <= 0) and (civ2.dev > 0): #civ1 is dead and civ2 is alive
+                data[10].append("Civ 2 Annihilates Civ 1")
+            elif (civ2.dev <=0) and (civ1.dev > 0): #civ2 is dead and civ1 is alive
+                data[10].append("Civ 1 Annihilates Civ 2")
+            elif (civ2.dev <=0) and (civ1.dev <= 0): #everyone's dead
+                data[10].append("Everyone Has Died")
+            elif (currentRound >= 10*attackRounds): #we've timed out
+                data[10].append("Stalemate")
+            elif (consecutiveComm >= attackRounds): #we've established trust
+                data[10].append("Trust Established")
+            else:
+                data[10].append("Error Encountered")
+
+            if(param=="dist"):
+                break
+
+    np_array=np.transpose(np.asarray(data))
+    pd.DataFrame(np_array).to_csv("./"+param+"_data.csv")
+
 
 def getData():
-    data=[["civ1 dev"],["civ1 arms"],["civ1 agg"],["civ1 com"],["civ2 dev"],["civ2 arms"],["civ2 agg"],["civ2 com"],["dist"],["rounds"],["outcome"]]
-    for i in range(100):
-        if(i%10==0):
-            print(i)
-        civInputs = GetRandomCivSettings()
-        civ1 = civilization.civ(civInputs["civ1"]["dev"], civInputs["civ1"]["arms"], civInputs["civ1"]["agg"], civInputs["civ1"]["comm"]) #init civ 1 using values (default or user input)
-        civ2 = civilization.civ(civInputs["civ2"]["dev"], civInputs["civ2"]["arms"], civInputs["civ2"]["agg"], civInputs["civ2"]["comm"]) #init civ 2 using values (default or user input)
-        
-        civDistance = i//10
+##    t_dist=threading.Thread(target=runTrials, args=("dist",))
+##    t_dev=threading.Thread(target=runTrials, args=("dev",))
+##    t_arms=threading.Thread(target=runTrials, args=("arms",))
+##    t_agg=threading.Thread(target=runTrials, args=("agg",))
+##    t_comm=threading.Thread(target=runTrials, args=("comm",))
+##
+##    t_dist.start()
+##    t_dev.start()
+##    t_arms.start()
+##    t_agg.start()
+##    t_comm.start()
+##
+##    t_dist.join()
+##    t_dev.join()
+##    t_arms.join()
+##    t_agg.join()
+##    t_comm.join()
 
-        data[0].append(civ1.dev)
-        data[1].append(civ1.arms)
-        data[2].append(civ1.agg)
-        data[3].append(civ1.comm)
-
-        data[4].append(civ2.dev)
-        data[5].append(civ2.arms)
-        data[6].append(civ2.agg)
-        data[7].append(civ2.comm)
-
-        data[8].append(civDistance)
-        
-        currentRound = 0
-        consecutiveComm = 0
-        attackRounds = 0 #instantiate number of rounds
-        if (civ1.arms < civ2.arms): #if civ 1 is slower than civ 2
-            attackRounds = math.ceil(civDistance/civ1.arms) #rounds for the slower civ to nuke the faster civ
-        else: #otherwise civ 2 is slower
-            attackRounds = math.ceil(civDistance/civ2.arms) #same as above
-        #this while condition is really long:
-        #checks that neither civ is dead, we haven't timed out, and we haven't established trust
-        while (civ1.dev > 0) and (civ2.dev > 0) and (currentRound < 10*attackRounds) and (consecutiveComm < attackRounds):
-            #check to see if any pending actions are realized this turn
-            civ1Actionable = filter(interaction.CheckPending, civ1.pending) #find the items that are happening this turn
-            for action in civ1Actionable: #for each of those items
-                civ1.dev += action["impact"] #add their impact (pos or neg) to development
-            civ2Actionable = filter(interaction.CheckPending, civ2.pending) #do same steps for civ 2
-            for action in civ2Actionable:
-                civ2.dev += action["impact"]
-                
-            #once development scores are altered, decide what to do next
-            est = decision.ChooseActions(civ1, civ2) #get the decisions of each civ
-            
-            #keep track of consecutive communication count
-            if ((est[0] == "communicate") and (est[1] == "communicate")): #if both communicated
-                consecutiveComm += 1 #increase their comm count
-            else: #if one attacked
-                consecutiveComm = 0 #zero out comm count
-                
-            #handle actions for this turn
-            if (est[0] == "attack"): #civ 1 attacked
-                civ2.pending.append(interaction.Attack(civ1, civDistance)) #add an attack to civ 2's pending
-            else: #civ 1 communicated
-                civ2.pending.append(interaction.Communicate(civ1, civDistance)) #add a communication to civ 2's pending
-            if(est[1] == "attack"): #civ 2 attacked
-                civ1.pending.append(interaction.Attack(civ2, civDistance)) #add an attack to civ 1's pending
-            else:
-                civ1.pending.append(interaction.Communicate(civ2, civDistance)) #add a communication to civ 1's pending
-                
-            currentRound += 1 #increment current round for timeout
-                
-        #the game has concluded, figure out why
-        data[9].append(currentRound)
-        if (civ1.dev <= 0) and (civ2.dev > 0): #civ1 is dead and civ2 is alive
-            data[10].append("Civ 2 Annihilates Civ 1")
-        elif (civ2.dev <=0) and (civ1.dev > 0): #civ2 is dead and civ1 is alive
-            data[10].append("Civ 1 Annihilates Civ 2")
-        elif (civ2.dev <=0) and (civ1.dev <= 0): #everyone's dead
-            data[10].append("Everyone Has Died")
-        elif (currentRound >= 10*attackRounds): #we've timed out
-            data[10].append("Stalemate")
-        elif (consecutiveComm >= attackRounds): #we've established trust
-            data[10].append("Trust Established")
-        else:
-            data[10].append("Error Encountered")
-
-    np_array=np.asarray(data)
-    pd.DataFrame(np_array).to_csv("./data.csv")
+    runTrials("randomize")
+ 
             
         
 getData()
---
+++ code/seti_data_wrangling.R
@@ -0,0 +1,20 @@
+dist_data<-read.csv("./Desktop/Logan/Smith/Senior\ Year/Game\ Theory/Project\ 3/code/dist_data.csv")
+
+agg_data<-read.csv("./Desktop/Logan/Smith/Senior\ Year/Game\ Theory/Project\ 3/code/agg_data.csv")
+
+comm_data<-read.csv("./Desktop/Logan/Smith/Senior\ Year/Game\ Theory/Project\ 3/code/comm_data.csv")
+
+dev_data<-read.csv("./Desktop/Logan/Smith/Senior\ Year/Game\ Theory/Project\ 3/code/dev_data.csv")
+
+arms_data<-read.csv("./Desktop/Logan/Smith/Senior\ Year/Game\ Theory/Project\ 3/code/arms_data.csv")
+
+ggplot(agg_data, aes(x=civ1.agg, y=civ2.agg, color=outcome))+
+  geom_point()
+
+
+ggplot(comm_data, aes(x=civ1.com, y=civ2.com, color=outcome))+
+  geom_point()
+
+ggplot(dist_data, aes(x=dist, y=rounds, color=outcome))+
+  geom_point()+
+  geom_jitter()
