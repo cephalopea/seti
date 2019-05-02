1c8af265447339582103e151dd8663769f2c67c6
pterodactylogan <loganswan37@gmail.com>
2019-05-01 23:28:22 -0400

    update R file

--- code/seti_data_wrangling.R
+++ code/seti_data_wrangling.R
@@ -8,9 +8,10 @@ dev_data<-read.csv("./Desktop/Logan/Smith/Senior\ Year/Game\ Theory/Project\ 3/c
 
 arms_data<-read.csv("./Desktop/Logan/Smith/Senior\ Year/Game\ Theory/Project\ 3/code/arms_data.csv")
 
+random_data<-read.csv("./Desktop/Logan/Smith/Senior\ Year/Game\ Theory/Project\ 3/code/randomize_data.csv")
+
 ggplot(agg_data, aes(x=civ1.agg, y=civ2.agg, color=outcome))+
   geom_point()
-
 
 ggplot(comm_data, aes(x=civ1.com, y=civ2.com, color=outcome))+
   geom_point()
@@ -18,3 +19,15 @@ ggplot(comm_data, aes(x=civ1.com, y=civ2.com, color=outcome))+
 ggplot(dist_data, aes(x=dist, y=rounds, color=outcome))+
   geom_point()+
   geom_jitter()
+
+ggplot(dev_data, aes(x=civ1.dev, y=civ2.dev, color=outcome))+
+  geom_point()
+
+ggplot(arms_data, aes(x=civ1.arms, y=civ2.arms, color=outcome))+
+  geom_point()
+
+ggplot(random_data %>% filter(rounds<500), aes(x=dist,y=rounds,color=outcome))+
+  geom_point()+
+  geom_jitter()
+
+
